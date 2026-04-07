e45c97649bf12550d9c32caaf1bcaae6784e52aa
Lucas Bottino <lucasgabottino@gmail.com>
2026-04-07 16:42:41 -0300

    counter: add ip address for clusters

--- src/counter.c
+++ src/counter.c
@@ -2,11 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+void get_ip(char *hostname, char *ip) 
+{
+    struct hostent *he;
+    struct in_addr **addr_list;
+
+    if ((he = gethostbyname(hostname)) == NULL) 
+    {
+        strcpy(ip, "unknown");
+        return;
+    }
+
+    addr_list = (struct in_addr **) he->h_addr_list;
+
+    if (addr_list[0] != NULL) strcpy(ip, inet_ntoa(*addr_list[0]));
+    else strcpy(ip, "unknown");
+}
 
 int main(int argc, char *argv[]) 
 {
     int rank, size, N, token;
+    
     char hostname[256];
+    char ip[100];
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -14,13 +36,14 @@ int main(int argc, char *argv[])
 
     N = (argc > 1) ? atoi(argv[1]) : 10;
     gethostname(hostname, sizeof(hostname));
+    get_ip(hostname, ip);
 
     if (rank == 0 && N < size) fprintf(stderr, "Aviso: N=%d < processos=%d; nem todos imprimirao.\n", N, size);
 
     if (rank == 0)
     {
         token = 0;
-        printf("[proc %d | %s] -> %d\n", rank, hostname, token);
+        printf("[proc %d | %s | %s] -> %d\n", rank, hostname, ip, token);
         fflush(stdout);
         token = 1;
         MPI_Send(&token, 1, MPI_INT, 1 % size, 0, MPI_COMM_WORLD);
@@ -29,7 +52,7 @@ int main(int argc, char *argv[])
         {
             MPI_Recv(&token, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             if (token>N) break;
-            printf("[proc %d | %s] -> %d\n", rank, hostname, token);
+            printf("[proc %d | %s | %s] -> %d\n", rank, hostname, ip, token);
             fflush(stdout);
             token++;
             int next = (size==1) ? 0 : 1;
@@ -55,7 +78,7 @@ int main(int argc, char *argv[])
                 MPI_Send(&token, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
                 break;
             }
-            printf("[proc %d | %s] -> %d\n", rank, hostname, token);
+            printf("[proc %d | %s | %s] -> %d\n", rank, hostname, ip, token);
             fflush(stdout);
             token++;
             MPI_Send(&token, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
